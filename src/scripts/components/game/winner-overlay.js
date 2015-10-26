895f97cdfe9e0392c5356f1f120c9f98673aede0
oportocala <oportocala@gmail.com>
2015-10-26 16:21:07 +0200

    cleanup of win screen

--- src/scripts/components/game/winner-overlay.js
+++ src/scripts/components/game/winner-overlay.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReplayBtn from './overlay/replay-btn';
+import Player from './current-player/player';
 import GameActions from '../../actions/game';
 
 class WinnerOverlay extends React.Component {
@@ -9,6 +10,7 @@ class WinnerOverlay extends React.Component {
         return (<div className={className}>
             <div><img src={`images/player_${this.props.player}_win.svg`} height="85" /></div>
             <ReplayBtn />
+            <Player className={`player-${this.props.player} active`}/>
         </div>);
 
     }
